6715fbcd38736b4ded8faabbdf1be71f0bd78c01
amitroy4 <amitroyrock6071@gmail.com>
2023-11-14 22:38:28 +0600

    api routes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express();
 const dbConnection = require("./config/dbConfig")
+const routes = require("./routes")
 app.use(express.json());
 dbConnection()
+app.use(routes)
 
 app.listen(8000, function () {
     console.log("server is running");
