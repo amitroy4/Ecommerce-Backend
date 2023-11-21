f67034a1cb0f3683d62a249f066211154e0c4cde
amitroy4 <amitroyrock6071@gmail.com>
2023-11-21 13:26:18 +0600

    otp verification and error msg done

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const _ = express.Router();
 const registrationController = require("../../controllers/registrationController")
+const otpController = require("../../controllers/otpController")
 
 _.post("/registration", registrationController)
+_.post("/otpverify", otpController)
 // _.get("/registration", function (req, res) {
 //     res.send("done")
 // })
