ab373dcbfa97864420c7c71eb9bd2e3a21056e45
amitroy4 <amitroyrock6071@gmail.com>
2023-11-29 13:21:01 +0600

    login api

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const _ = express.Router();
 const registrationController = require("../../controllers/registrationController")
-const otpController = require("../../controllers/otpController")
+const otpController = require("../../controllers/otpController");
+const loginController = require('../../controllers/loginController');
 
 _.post("/registration", registrationController)
 _.post("/otpverify", otpController)
+_.post("/login", loginController)
 // _.get("/registration", function (req, res) {
 //     res.send("done")
 // })
